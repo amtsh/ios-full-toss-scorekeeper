7dc3479503c5f9026728107a2d892c9c0af78f61
Amit Shinde <ashinde@spotify.com>
2023-05-15 22:33:05 +0200

    Initial Commit

---
+++ FullToss/FullTossApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullTossApp.swift
+//  FullToss
+//
+//  Created by Amit Shinde on 2023-05-15.
+//
+
+import SwiftUI
+
+@main
+struct FullTossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FullTossUITests/FullTossUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FullTossUITestsLaunchTests.swift
+//  FullTossUITests
+//
+//  Created by Amit Shinde on 2023-05-15.
+//
+
+import XCTest
+
+final class FullTossUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
