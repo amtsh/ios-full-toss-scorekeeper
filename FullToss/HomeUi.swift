acadecbc6bb0675adecf08dbdce396de25db3877
Amit Shinde <shinde13amit@gmail.com>
2023-05-17 13:28:07 +0200

    TeamA UI Complete

--- FullToss/HomeUi.swift
+++ FullToss/HomeUi.swift
@@ -8,60 +8,59 @@
 import SwiftUI
 
 struct HomeUi: View {
-    var body: some View {
+  var body: some View {
+    VStack {
+      ScrollView(showsIndicators: false) {
         VStack {
-            ScrollView(showsIndicators: false) {
-                VStack {
-                    Text("Full Toss")
-                        .font(.system(.largeTitle, weight: .bold))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .clipped()
-                        
-                    ForEach(0..<5) { i in // Replace with your data model here
-                        Divider()
-                            .opacity(0.5)
-                        HStack(spacing: 0) {
-                            VStack {
-                                Text("Cricket Match \(i+1)")
-                                    .font(.headline)
-                                    .padding(.bottom, 2)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .clipped()
-                                Text("30 May 2023")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .clipped()
-                                    .foregroundColor(.secondary)
-                            }
-                            Spacer()
-                            Image(systemName: "ellipsis")
-                        }
-                        .padding(.vertical)
-                        .font(.subheadline)
-                    }
-                }
+          Text("Full Toss")
+            .font(.system(.largeTitle, weight: .bold))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .clipped()
+
+          ForEach(0..<5) { i in // Replace with your data model here
+            Divider()
+              .opacity(0.5)
+            HStack(spacing: 0) {
+              VStack {
+                Text("Cricket Match \(i+1)")
+                  .font(.headline)
+                  .padding(.bottom, 2)
+                  .frame(maxWidth: .infinity, alignment: .leading)
+                  .clipped()
+                Text("30 May 2023")
+                  .frame(maxWidth: .infinity, alignment: .leading)
+                  .clipped()
+                  .foregroundColor(.secondary)
+              }
+              Spacer()
+              Image(systemName: "ellipsis")
             }
-            Spacer()
-            Text("Start New Match")
-                .font(.system(.callout, weight: .semibold))
-                .padding()
-                .frame(maxWidth: .infinity)
-                .clipped()
-                .foregroundColor(.white)
-                .background(.blue)
-                .mask { RoundedRectangle(cornerRadius: 16, style: .continuous) }
-                .padding(.bottom, 60)
+            .padding(.vertical)
+            .font(.subheadline)
+          }
         }
+      }
+      Spacer()
+      Text("New Match")
+        .font(.system(.callout, weight: .semibold))
+        .padding()
         .frame(maxWidth: .infinity)
         .clipped()
-        .padding(.top, 53)
-        .padding(.bottom, 0)
-        .padding(.horizontal, 29)
+        .foregroundColor(.white)
+        .background(.blue)
+        .mask { RoundedRectangle(cornerRadius: 16, style: .continuous) }
+        .padding(.bottom, 60)
     }
+    .frame(maxWidth: .infinity)
+    .clipped()
+    .padding(.top, 53)
+    .padding(.bottom, 0)
+    .padding(.horizontal, 29)
+  }
 }
 
 struct HomeUi_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeUi()
-    }
+  static var previews: some View {
+    HomeUi()
+  }
 }
-
--- FullToss/TeamAUi.swift
+++ FullToss/TeamAUi.swift
@@ -8,158 +8,145 @@
 import SwiftUI
 
 struct TeamAUi: View {
-    var body: some View {
-        ScrollView(showsIndicators: false) {
+  var body: some View {
+    VStack {
+      VStack {
+        VStack {
+          ScrollView(showsIndicators: false) {
             VStack {
-                Group {
-                    HStack {
-                        Text("Team A")
-                            .font(.headline)
-                            .foregroundColor(.green)
-                        Spacer()
-                    }
-                    .padding(.top, 16)
-                    .padding(.horizontal)
-                    Text("22 / 1")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                        .clipped()
-                        .padding(.horizontal)
-                        .font(.system(size: 70, weight: .black, design: .default))
-                    Text("(3.2 / 10)")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                        .clipped()
-                        .padding(.horizontal)
-                        .font(.system(.title, weight: .bold))
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    HStack {
-                        Text("Run rate")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .multilineTextAlignment(.trailing)
-                        Text("4.2")
-                            .font(.system(.subheadline, weight: .medium))
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .clipped()
-                    HStack {
-                        Text("Projected Score")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .multilineTextAlignment(.trailing)
-                        Text("65")
-                            .font(.system(.subheadline, weight: .medium))
-                    }
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .clipped()
-                    .padding(.bottom)
-                    HStack {
-                        Text("Extras")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .multilineTextAlignment(.trailing)
-                        Text("2 wides . 4 no balls")
-                            .font(.system(.subheadline, weight: .medium))
-                    }
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .clipped()
+              HStack() {
+                Text("TEAM A")
+                  .font(.system(.headline, weight: .bold))
+                  .foregroundColor(.green)
+                Spacer()
+                Text("22 - 1")
+                  .frame(maxWidth: .infinity, alignment: .trailing)
+                  .clipped()
+                  .font(.system(size: 60, weight: .regular, design: .default))
+              }
+              .padding(.top, 16)
+              .padding(.horizontal)
+              Text("(3.2 / 10)")
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .clipped()
+                .padding(.horizontal)
+                .font(.system(.title, weight: .regular))
+                .foregroundColor(.primary)
+              VStack {
+                HStack {
+                  Text("Run rate")
+                  Text("4.2")
                 }
-                Group {
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Divider()
-                    HStack {
-                        Text("Team B")
-                            .font(.system(.headline, weight: .medium))
-                            .foregroundColor(Color(.tertiaryLabel))
-                        Spacer()
-                        Text("Yet to bat")
-                            .font(.system(.subheadline, weight: .medium))
-                            .foregroundColor(Color(.tertiaryLabel))
-                    }
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .clipped()
-                    Divider()
-                    HStack {
-                        Text("This over")
-                            .font(.system(.caption, weight: .semibold))
-                            .foregroundColor(.secondary)
-                        Spacer()
-                        Text("13 runs (1 ball left)")
-                            .font(.system(.caption, weight: .medium))
-                            .foregroundColor(.secondary)
-                    }
-                    .padding(.top)
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .clipped()
-                    LazyHGrid(rows: [GridItem(.flexible(), spacing: 0, alignment: .center)], spacing: 50) {
-                        ForEach(0..<5) { _ in // Replace with your data model here
-                            Text("1")
-                                .font(.system(.title, weight: .black))
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    /* HStack {
-                        Text("Undo")
-                            .font(.system(.footnote, design: .rounded, weight: .semibold))
-                            .padding(.vertical, 3)
-                            .padding(.horizontal, 7)
-                            .background {
-                                Rectangle()
-                                    .fill(.clear)
-                                    .background(Material.regular)
-                                    .mask {
-                                        RoundedRectangle(cornerRadius: 16, style: .continuous)
-                                    }
-                            }
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .clipped()
-                        Text("Redo")
-                            .font(.system(.footnote, design: .rounded, weight: .semibold))
-                            .padding(.vertical, 3)
-                            .padding(.horizontal, 7)
-                            .background {
-                                Rectangle()
-                                    .fill(.clear)
-                                    .background(Material.regular)
-                                    .mask {
-                                        RoundedRectangle(cornerRadius: 16, style: .continuous)
-                                    }
-                            }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .clipped()
-                    }
+                .padding(.horizontal)
+                .padding(.bottom)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .clipped()
+                .foregroundColor(.secondary)
+                .font(.system(.caption, weight: .semibold))
+                HStack {
+                  Text("Projected Score")
+                  Text("65")
                 }
-                Group {
-                    Circle()
-                        .stroke(Color(.tertiaryLabel), lineWidth: 111)
-                        .background(Circle().fill(Color(.quaternaryLabel)))
-                        .scaleEffect(0.7, anchor: .center)
-                        .overlay {
-                            Image(systemName: "circle.inset.filled")
-                                .imageScale(.large)
-                                .symbolRenderingMode(.monochrome)
-                                .font(.largeTitle)
-                                .foregroundColor(.secondary)
-                        } **/
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .clipped()
+                .padding(.bottom)
+                .foregroundColor(.secondary)
+                .font(.system(.caption, weight: .semibold))
+                HStack {
+                  Text("Extras")
+                  Text("2 wides . 4 no balls")
                 }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .clipped()
+                .foregroundColor(.secondary)
+                .font(.system(.caption, weight: .semibold))
+              }
+              .padding(.top, 1)
             }
+          }
+          Divider()
+          HStack {
+            Text("TEAM B")
+              .font(.system(.subheadline, weight: .medium))
+              .foregroundColor(Color(.tertiaryLabel))
+            Spacer()
+            Text("Yet to bat")
+              .font(.system(.subheadline, weight: .medium))
+              .foregroundColor(Color(.tertiaryLabel))
+          }
+          .padding(.horizontal)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .clipped()
+          Divider()
+          HStack {
+            Text("This over")
+              .font(.system(.caption, weight: .semibold))
+              .foregroundColor(.secondary)
+            Spacer()
+            Text("13 runs (1 ball left)")
+              .font(.system(.caption, weight: .medium))
+              .foregroundColor(.secondary)
+          }
+          .padding(.top)
+          .padding(.horizontal)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .clipped()
+          LazyHGrid(rows: [GridItem(.flexible(), spacing: 0, alignment: .center)], spacing: 50) {
+            Text("4")
+              .font(.system(.largeTitle, weight: .semibold))
+              .foregroundColor(.secondary)
+            Text("2")
+              .font(.system(.largeTitle, weight: .semibold))
+              .foregroundColor(.secondary)
+            Text("6")
+              .font(.system(.largeTitle, weight: .semibold))
+              .foregroundColor(.secondary)
+            Text("W")
+              .font(.system(.largeTitle, weight: .semibold))
+              .foregroundColor(.secondary)
+            Text("1")
+              .font(.system(.largeTitle, weight: .semibold))
+              .foregroundColor(.secondary)
+          }
         }
+        LazyHGrid(rows: [GridItem(.flexible(), alignment: .center), GridItem(.flexible(), alignment: .center), GridItem(.flexible(), alignment: .center), GridItem(.flexible(), alignment: .center)], spacing: 40) {
+          
+          Group {
+            ScoreInputText(value: "4")
+            ScoreInputText(value: "2")
+            ScoreInputText(value: "0")
+            
+            ScoreInputIcon(value: "arrow.uturn.left")
+              .imageScale(.large)
+            
+            ScoreInputText(value: "6")
+            ScoreInputText(value: "3")
+            ScoreInputText(value: "1")
+            
+            
+            ScoreInputIcon(value: "arrow.uturn.right")
+              .imageScale(.large)
+            
+            ScoreInputText(value: "W", opacity: 0.15, textColor: Color.red)
+            ScoreInputText(value: "NB", opacity: 0.60, textColor: Color.black)
+          }
+          Group {
+            ScoreInputText(value: "WB", opacity: 0.60, textColor: Color.black)
+            ScoreInputText(value: "OK", textColor: .green)
+          }
+          
+        }
+        .padding()
+      }
     }
+  }
 }
 
 struct TeamAUi_Previews: PreviewProvider {
-    static var previews: some View {
-        TeamAUi()
-    }
+  static var previews: some View {
+    TeamAUi()
+  }
 }
+
